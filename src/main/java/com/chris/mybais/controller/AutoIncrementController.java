b31479d6dd92f4049333a640f695640d889c4852
ChrisLeGitHub <mrchriscl@icloud.com>
2017-11-29 15:28:02 +0800

    完成增删改查

--- src/main/java/com/chris/mybais/controller/AutoIncrementController.java
+++ src/main/java/com/chris/mybais/controller/AutoIncrementController.java
@@ -1,5 +1,6 @@
 package com.chris.mybais.controller;
 
+import com.chris.mybais.http.HttpUtils;
 import com.chris.mybais.po.AutoIncrementEntity;
 import com.chris.mybais.service.AutoIncrementService;
 
@@ -29,7 +30,7 @@ public class AutoIncrementController {
     @ResponseBody
     public ResponseEntity<String> insertData(@ModelAttribute AutoIncrementEntity autoIncrementEntity) throws Exception {
         int i = mAutoIncrementService.insertData(autoIncrementEntity);
-        if (i==1){
+        if (i == 1) {
             i = 200;
         }
         JSONObject json = new JSONObject();
@@ -61,7 +62,7 @@ public class AutoIncrementController {
     @ResponseBody
     public ResponseEntity<String> deleteData(@RequestParam Long id) throws Exception {
         int i = mAutoIncrementService.deleteData(id);
-        if (i==1){
+        if (i == 1) {
             i = 200;
         }
         JSONObject json = new JSONObject();
@@ -77,13 +78,15 @@ public class AutoIncrementController {
     @RequestMapping(value = "/updatedata", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ResponseEntity<String> updateData(@ModelAttribute AutoIncrementEntity autoIncrementEntity) throws Exception {
-        int i =  mAutoIncrementService.updateData(autoIncrementEntity);
-        if (i==1){
+        int i = mAutoIncrementService.updateData(autoIncrementEntity);
+        if (i == 1) {
             i = 200;
         }
+        String message = sendPost();
         JSONObject json = new JSONObject();
         try {
             json.put("result", i);
+            json.put("message",message);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -91,4 +94,15 @@ public class AutoIncrementController {
         return responseEntity;
     }
 
+    public String sendPost() {
+        //发送 POST 请求
+        String sr = HttpUtils.sendPost("http://10.11.26.24:8080/autoincrement/selectdata", "id=3");
+        return sr;
+    }
+
+    public String sendGet() {
+        //发送 GET 请求
+        String s = HttpUtils.sendGet("http://localhost:6144/Home/RequestString", "key=123&v=456");
+        return s;
+    }
 }
